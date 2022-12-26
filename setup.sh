67c43c602c7af43d25676d6174cdada3ce1b5ea3
Jean-Francois Chenier <jean-francois.chenier@sparkmicro.com>
2022-12-26 16:39:12 -0500

    Create setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+pacman -Syy
+pacman -S xorg networkmanager --noconfirm
+pacman -S gnome --noconfirm
+systemctl enable gdm.service
+systemctl enable NetworkManager.service
+# pacman -S refind --noconfirm
+
+# refind-install
+pacman -S snapper snap-pac --noconfirm
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+echo \
+"[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+pacman -Syy
+pacman -S grub-btrfs inotify-tools --noconfirm
+pacman -S btrfs-assistant --noconfirm
+snapper -c root create-config /
+systemctl enable grub-btrfsd
+pacman -S pamac-aur --noconfirm
+reboot now
