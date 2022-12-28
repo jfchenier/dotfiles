b5a201897aab8a1fee6510ae35c656e7b885e7a4
Jean-Francois Chenier <jean-francois.chenier@sparkmicro.com>
2022-12-28 13:13:56 -0500

    Add extensions configuration

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-#/bin/bash
+#!/bin/bash
 pacman -Syy
 pacman -S xorg networkmanager --noconfirm
 pacman -S gnome gnome-tweaks --noconfirm
+pacman -Syu gnome-shell-extension-appindicator
 systemctl enable gdm.service
 systemctl enable NetworkManager.service
 # pacman -S refind --noconfirm
@@ -21,15 +22,22 @@ pacman -S btrfs-assistant --noconfirm
 snapper -c root create-config /
 systemctl enable grub-btrfsd
 
-pacman -S zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k lsd--noconfirm
+pacman -S zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k lsd --noconfirm
 cp .zshrc ~/
 chsh -s /usr/bin/zsh $USER
 
-sudo pacman -S catppuccin-gtk-theme-mocha catppuccin-wallpapers-git catppuccin-mocha-grub-theme-git tela-icon-theme-git
+pacman -S gnome-browser-connector
+pacman -S catppuccin-gtk-theme-mocha catppuccin-wallpapers-git catppuccin-mocha-grub-theme-git tela-icon-theme-git --noconfirm
 cp zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
+dconf write /org/gnome/desktop/interface/monospace-font-name "'MesloLGS NF 10'"
 
-cp -r Catppuccin-Mocha-Standard-Lavender-Dark/ ~/.local/share/themes
+mkdir -p ~/.local/share/themes/ && cp -r Catppuccin-Mocha-Standard-Lavender-Dark/ ~/.local/share/themes/Catppuccin-Mocha-Standard-Lavender-Dark/
+mkdir -p ~/.config/gtk-4.0/ && cp Catppuccin-Mocha-Standard-Lavender-Dark/gtk-4.0/ ~/.config/gtk-4.0/
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 dconf write /org/gnome/shell/extensions/user-theme/name "'Catppuccin-Mocha-Standard-Lavender-Dark'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'Catppuccin-Mocha-Standard-Lavender-Dark'"
+
+
 
 pacman -S pamac-aur --noconfirm
 reboot now
