4fe6fa5a1b4596cadf977524519c9bf51a36caae
Jean-Francois Chenier <jean-francois.chenier@sparkmicro.com>
2023-01-01 12:25:42 -0500

    Update gnome-shell extensions settings

--- configure-theme.sh
+++ configure-theme.sh
@@ -8,7 +8,7 @@ cp zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~
 mkdir -p ~/.local/share/themes/ && cp -r Catppuccin-Mocha-Standard-Lavender-Dark/ ~/.local/share/themes/Catppuccin-Mocha-Standard-Lavender-Dark/
 cp -r Catppuccin-Mocha-Standard-Lavender-Dark/gtk-4.0/ ~/.config/
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-dconf write /org/gnome/desktop/interface/monospace-font-name "'MesloLGS NF 10'"
+dconf write /org/gnome/desktop/interface/monospace-font-name "'Hack Nerd Font Mono 12'"
 dconf write /org/gnome/shell/extensions/user-theme/name "'Catppuccin-Mocha-Standard-Lavender-Dark'"
 dconf write /org/gnome/desktop/interface/gtk-theme "'Catppuccin-Mocha-Standard-Lavender-Dark'"
 dconf write /org/gnome/desktop/interface/icon-theme "'Tela-dracula-dark'"
