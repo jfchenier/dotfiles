b1d9daeb1ba6b6ad946861803007322cfa142a0c
jfchenier <jfrancois.chenier@gmail.com>
2024-01-17 15:40:05 -0500

    Update chaotic-aur installation

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@ pacman -Syy
 pacman -S gnome gnome-tweaks gnome-shell-extension-appindicator --noconfirm
 pacman -S snapper snap-pac --noconfirm
 
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 echo \
 "[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
